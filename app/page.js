8fd71046ae854b5395551a8c022aacd10d474ff1
Bartłomiej Szkodny <118225391+BartlomiejSzkodny@users.noreply.github.com>
2023-10-17 00:09:04 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 //make text "Kocham" beatiful
 
 const Page = () => {
-    return /*#__PURE__*/React.createElement("div", null, "Kocham");
+    return /*#__PURE__*/React.createElement("div", null, "Kocham Olę");
     };
 //export page
 export default Page;
